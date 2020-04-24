a1687984b6bb31e0f368d39743ce514ba574d8a7
rhrhunter <rh.rhunter@gmail.com>
2020-04-24 19:10:11 +0000

    Custom start scripts

--- start_vpn.sh
+++ start_vpn.sh
@@ -2,11 +2,10 @@
 
 # spawn the server
 docker run --name ipsec-vpn-server \
-       --env-file ./custom-vpn.env \
+       --env-file ./vpn.env.example \
        --restart=always \
        -p 500:500/udp \
        -p 4500:4500/udp \
-       -v /lib/modules:/lib/modules:ro \
        -d --privileged \
        hwdsl2/ipsec-vpn-server
 
